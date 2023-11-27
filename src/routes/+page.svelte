0ec1d0dfc0bebef9f6af213c3a107b2be0381bbd
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-11-27 11:17:20 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+  export let data;
+  let { League } = data;
+</script>
+
+<ul>
+  {#each data.League as League}
+    <li>{League.name_champion}
+      Does this champion do true damage?({League.true_damage})
+    </li>
+  {/each}
+</ul>
+

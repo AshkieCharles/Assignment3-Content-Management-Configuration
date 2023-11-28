f75745d302f41f71204aa0b4fb38e097afa53a1c
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-11-28 08:56:17 -0700

    added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,29 +4,33 @@
   let { blog_post } = data;
 </script>
 
-
-
-
-
-<ul>
   {#each data.blog_post as blog_post}
     <h1>{blog_post.title_names}
     </h1>
-    <h2>{blog_post.author_name}</h2>
+    <h2>Created by: {blog_post.author_name}</h2>
+    <h3>
+      Published on {blog_post.date_time}
+      {blog_post.date}
+    </h3>
     <div>
       <p>
         {blog_post.content_body}
       </p>
     </div>
     <div>
+      <h2>
+        Summary
+      </h2>
       <p>
         {blog_post.summary}
       </p>
     </div>
+    <h3>
+      TAGS:
+    </h3> 
     <p>
-      {blog_post.blog_tags}
+      {blog_post.blog_tags }
     </p>
 
-  {/each}
-</ul>
 
+  {/each}
